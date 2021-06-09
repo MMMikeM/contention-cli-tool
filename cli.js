1438a3cc646d5ea059371025a609cf541ad4e47e
Mike <mikem.tba@gmail.com>
2021-06-09 20:31:43 +0200

    Final updates

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 const { calculate } = require("./src/calculator");
 const args = process.argv.slice(2);
-const filepath = "./linetags";
+const filepath = "/home/linetags";
+const ObjectsToCsv = require('objects-to-csv');
+var Table = require('cli-table');
 
 let soNumber = args[0];
 let city = args[1];
@@ -13,9 +15,69 @@ if (soNumber === undefined || city === undefined || carrier === undefined) {
 } else {
   let result = calculate(filepath, soNumber, city, carrier);
 
+  console.log("");
+
   console.log(
     `XC Speed: ${result.xcSpeed} - Sold Speed: ${
       result.soldSpeed
     } - Contention Ratio: 1:${parseFloat(result.contentionRatio).toFixed(2)}`
   );
+
+  const save = async () => {
+    const dummyRecord = {fileName: "XXXXXXXX",
+    speed: "XXXXXXXX",
+    // circuitType: circuitType || "",
+    circuitNumber: "XXXXXXXX",
+    carrier: "XXXXXXXX",
+    city: "XXXXXXXX"
+  }
+    const data = [result.xcData].concat([dummyRecord]).concat(result.speeddata)
+    
+    if (args.includes('-v')){
+      var table = new Table({
+        chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+               , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+               , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+               , 'right': '' , 'right-mid': '' , 'middle': '\t\t' },
+        style: { 'padding-left': 0, 'padding-right': 0 }
+      });
+
+      table.push(["FileName", "Speed", "CircuitNumber", "Carrier", "City"])
+
+      table.push([result.xcData.fileName, result.xcData.speed, result.xcData.circuitNumber, result.xcData.carrier, result.xcData.city])
+      table.push([''])
+      result.speeddata.map((item) => table.push([item.fileName, item.speed, item.circuitNumber, item.carrier, item.city]))
+      table.push([''])
+      table.push(["Total", result.soldSpeed, "", "", ""])
+
+
+      console.log("");
+      console.log(table.toString());
+      console.log("");
+
+
+
+
+    }
+
+    const os = require('os');
+
+    const csv = new ObjectsToCsv(data);
+
+    await csv.toDisk(`${os.homedir()}/${soNumber}-${city}-${carrier}.csv`);
+  }
+
+  save()
+
 }
+
+// enter xc SO number
+// grab speed
+// match all files based on cicrt_num_l1
+// spit out data used as csv
+// XC will always include xc in name
+// bitco-jhb-teraco-xc-01 should return 0
+// dfa-jhb-teraco-xc-01 should return a valid (and generate csv)
+// dfa-jhb-teraco-xc-02 should return a valid (and generate csv saying "not enough thingies")
+// return in CLI but also generate csv in "."
+// include sourcecode
--- src/calculator.js
+++ src/calculator.js
@@ -1,130 +1,171 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
+
 
 // INPUT: soNumber, city, carrier
 // Output: XC-speed, sold-speed, contentionRatio(1 decimal)
 
 const calculate = (linetagFolder, soNumber, city, carrier) => {
-  let soldSpeed = 0;
-  let contentionRatio = () => (soldSpeed / xcSpeed).toFixed(2);
 
-  let files = fs.readdirSync(linetagFolder);
-  let filteredFiles = files.filter((file) => file[0] !== ".");
+  let soldSpeed = 0
+  let contentionRatio = () => (soldSpeed / xcSpeed).toFixed(2)
+  let validFiles = []
+  let validXCFiles = []
 
-  let test = filteredFiles.map((file) =>
-    filterFiles(`${linetagFolder}/${file}`)
-  );
+  let files = fs.readdirSync(linetagFolder)
+  let filteredFiles = files.filter((file) => file[0] !== ".")
 
-  let xcData = filteredFiles.map((file) =>
-    extractData(`${linetagFolder}/${file}`)
-  );
+  filteredFiles.forEach((file) => {
+    const results = filterSO(`${linetagFolder}/${file}`, soNumber)
+    if (results) {
+      validFiles.push(results)
+    }
+  })
+
+  filteredFiles.forEach((file) => {
+    const results = filterXC(`${linetagFolder}/${file}`, "xc")
+    if (results) {
+      validXCFiles.push(results)
+    }
+  })
+
+  let xcData = validXCFiles.map((file) => {
+    let data = extractData(`${linetagFolder}/${file}`)
+    return(data)
+  })
+
+  let speedData = validFiles.map((file) => {
+    let data = extractData(`${linetagFolder}/${file}`)
+    return(data)
+  })
+
+  // console.log(xcData)
 
   let mainEntry = xcData.find(
     (entry) =>
-      entry.circuitType === "xc" &&
-      entry.carrier === carrier &&
-      entry.city === city
-  );
+      entry.circuitNumber.includes(soNumber) &&
+      entry.carrier === carrier
+  )
 
-  let xcSpeedString = mainEntry.speed || "";
-  let xcSpeed = unitHandler(xcSpeedString, mainEntry.fileName);
+  // console.log(speedData.includes(mainEntry))
+  // console.log(mainEntry)
+  // console.log(speedData)
 
-  let data = files
-    .filter((file) => file[0] !== ".")
-    .filter((file) => fs.readFileSync(file).contains("svsbackhaul"));
 
-  soldSpeed = data
-    .filter((entry) => entry.circuitType !== "xc")
-    .filter((entry) => entry.circuitNumber.includes(soNumber))
+  let xcSpeedString
+  let xcSpeed
+
+  if (mainEntry) {
+    xcSpeedString = mainEntry.speed || ""
+    xcSpeed = unitHandler(xcSpeedString, mainEntry.fileName)
+  }
+
+  soldSpeed = speedData
     .reduce((accumulator, entry) => {
-      let speedInt = unitHandler(entry.speed, entry.fileName);
-      return accumulator + speedInt;
-    }, 0);
+      let speedInt = unitHandler(entry.speed, entry.fileName)
+      return accumulator + speedInt
+    }, 0)
+
 
   return {
     xcSpeed: xcSpeedString,
     soldSpeed: `${soldSpeed}mbps`,
     contentionRatio: contentionRatio(),
-  };
-};
+    speeddata: speedData,
+    xcData: mainEntry
+  }
+}
 
 const unitHandler = (input, fileName) => {
-  let speedRegex = /\s?(\d+)(\w)bps/i;
+  let speedRegex = /\s?(\d+)(\w)(bps)?/i
+
   if (input === "") {
-    return 0;
+    return 0
   } else {
-    let [_, speed, unit] = speedRegex.exec(input);
-    let speedInt = parseInt(speed);
+    let [_, speed, unit] = speedRegex.exec(input)
+    let speedInt = parseInt(speed)
     if (unit === "g") {
-      speedInt = speedInt * 1000;
+      speedInt = speedInt * 1000
     }
     if (unit === "t") {
-      speedInt = speedInt * 1000 * 1000;
+      speedInt = speedInt * 1000 * 1000
     }
-    return speedInt;
+    return speedInt
   }
-};
+}
 
-const filterFiles = (filePath) => {
-  const file = fs.readFileSync(filePath, "utf8");
-  const lines = file.split("\n");
-  let fileName = path.basename(filePath);
-  console.log({
-    filePath: filePath,
-    file: lines.find((item) => item.match(/speed: (.*)/)),
-  });
-};
+const filterSO = (filePath, soNumber) => {
+  const file = fs.readFileSync(filePath, "utf8")
+  const lines = file.split("\n")
+  const searcher = lines.find((item) => item.match(/circt_num_l1: (.*)/)) || ""
+  const searcherXC = lines.find((item) => item.match(/circt_type: (.*)/)) || ""
+
+  if (searcher.includes(soNumber) && !searcherXC.includes("xc")) {
+    return path.basename(filePath)
+  } else {
+    return null
+  }
+}
+
+const filterXC = (filePath, XC) => {
+  const file = fs.readFileSync(filePath, "utf8")
+  const lines = file.split("\n")
+  const searcher = lines.find((item) => item.match(/circt_type: (.*)/)) || ""
+
+  if (searcher.includes(XC)) {
+    return path.basename(filePath)
+  } else {
+    return null
+  }
+}
 
 const extractData = (filePath) => {
-  const file = fs.readFileSync(filePath, "utf8");
-  const lines = file.split("\n");
-  let fileName = path.basename(filePath);
+  const file = fs.readFileSync(filePath, "utf8")
+  const lines = file.split("\n")
 
-  let speed, carrier, city, circuitType, circuitNumber;
-  let error = null;
+  let fileName = path.basename(filePath)
+
+  let speed, carrier, city, circuitType, circuitNumber
+  let error = null
 
   try {
     [speed, error] = regexer(/speed: (.*)/, lines, fileName);
     [carrier, error] = regexer(/circt_carrier: (.*)/, lines, fileName);
-    [city, error] = regexer(
-      /pswitch_host:\ssvs-za-(.*)-\w+-\w+-\d+/,
-      lines,
-      fileName
-    );
+    [city, error] = regexer(/pswitch_host:\ssvs-za-(.*)-\w+-\w+-\d+/, lines, fileName);
     [circuitType, error] = regexer(/circt_type: (.*)/, lines, fileName);
     [circuitNumber, error] = regexer(/circt_num_l1: (.*)/, lines, fileName);
+
+    let results = {fileName: fileName || "",
+    speed: speed || "",
+    // circuitType: circuitType || "",
+    circuitNumber: circuitNumber || "",
+    carrier: carrier || "",
+    city: city || ""
+  }
+
     if (error !== null) {
       throw new Error(`Regex failed on file: ${filePath} - ${error}`);
-      // console.log(error);
     }
+    return results
   } catch (error) {
-    console.log(error);
-  } finally {
-    return {
-      fileName: fileName,
-      speed: speed,
-      circuitType: circuitType,
-      circuitNumber: circuitNumber,
-      carrier: carrier,
-      city: city,
-    };
+    console.log(error)
   }
-};
+}
 
 const regexer = (pattern, lines, filePath) => {
-  let matchDirty = lines.find((line) => line.match(pattern));
-  let match = "";
+  let matchDirty = lines.find((line) => line.match(pattern))
+  let match
   if (matchDirty) {
-    match = pattern.exec(matchDirty);
-    return [match[1], null];
+    match = pattern.exec(matchDirty)
+    return [match[1], null]
   } else {
-    return ["", `No match found - ${pattern} in ${filePath}`];
+    return ["", `No match found - ${pattern} in ${filePath}`]
   }
-};
+}
 
 module.exports = {
   calculate: calculate,
   extractData: extractData,
   unitHandler: unitHandler,
   regexer: regexer,
-};
+}
